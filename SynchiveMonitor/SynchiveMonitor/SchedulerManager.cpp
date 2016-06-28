9f9c2e40e0050c54be8440b318afea989a0cc05a
nuttynuts17 <tonyhsu17@gmail.com>
2016-06-28 01:07:56 -0700

    comments and cleanup

--- SynchiveMonitor/SynchiveMonitor/SchedulerManager.cpp
+++ SynchiveMonitor/SynchiveMonitor/SchedulerManager.cpp
@@ -6,29 +6,86 @@ SchedulerManager::SchedulerManager()
 {
 	wchar_t buffer[1024];
 	GetModuleFileName(NULL, buffer, 1024);
-	
+
 	processPath = gcnew String(buffer);
 	Console::WriteLine(processPath);
 }
 
-String^ SchedulerManager::executeCommand(SchedulerManager::Query type, String^ path)
+// creates a new task in task scheduler that will run on login
+String^ SchedulerManager::newLocation(String^ path)
 {
-	if (path != nullptr)
+	validateMonitorFile();
+	String^ output = executeCommand(SchedulerManager::Query::newLocation, path);
+	return output;
+}
+
+// starts a single instance
+String^ SchedulerManager::oneTime(String^ path)
+{
+	validateMonitorFile();
+	String^ output = executeCommand(SchedulerManager::Query::oneTime, path);
+	return output;
+}
+
+// removes a specific location from task scheduler
+String^ SchedulerManager::removeLocation(String^ path)
+{
+	String^ output = executeCommand(SchedulerManager::Query::remove, path);
+	return output->Replace(kSlashReplacement, "\\");  // removes internal phrase when system spits task name back
+}
+
+// all scheduled task
+String^ SchedulerManager::removeAll()
+{
+	String^ output = "";
+	String^ toRemove = executeCommand(SchedulerManager::Query::list, nullptr);
+	ArrayList^ outputList = parseOutput(toRemove);
+
+	for each(String^ path in outputList)
 	{
-		path = path->Replace("\"", ""); // stripe out quoted path
-	}
-	if (path != nullptr && !Directory::Exists(path))
-	{
-		return "Invliad Directory!";
+		//Console::WriteLine(path);
+		output += executeCommand(SchedulerManager::Query::remove, path) + "\n";
 	}
 
-	String^ arguments = getQueryForType(type, path); // get arguments for task scheduler
-	
-	if (type == SchedulerManager::Query::oneTime)
+	return output->Replace(kSlashReplacement, "\\"); // removes internal phrase when system spits task name back
+}
+
+// list all scheduled task currently being monitored
+String ^ SchedulerManager::listLocations()
+{
+	String^ finalOutput = "";
+	String^ output = executeCommand(SchedulerManager::Query::list, nullptr);
+
+	ArrayList^ outputList = parseOutput(output);
+	for each(String^ str in outputList)
+	{
+		finalOutput += str + "\n";
+	}
+	return finalOutput;
+}
+
+// starts the task through commandline
+String^ SchedulerManager::executeCommand(SchedulerManager::Query type, String^ path)
+{
+	if(path != nullptr)
+	{
+		if(!Directory::Exists(path))
+		{
+			return "Invliad Directory!"; // short-circuit if invalid directory
+		}
+		else
+		{
+			path = path->Replace("\"", ""); // strip out quoted path
+		}
+	}
+
+	String^ arguments = getArgsForType(type, path); // get arguments for task scheduler
+
+	if(type == SchedulerManager::Query::oneTime)
 	{
 		Process^ p = gcnew Process();
-		ProcessStartInfo^ ps = gcnew ProcessStartInfo(kStoragePath + kFileName , "\"" + path + "\"");
-		//ps->WindowStyle = Diagnostics::ProcessWindowStyle::Hidden;
+		ProcessStartInfo^ ps = gcnew ProcessStartInfo(kStoragePath + kFileName, "\"" + path + "\"");
+		ps->WindowStyle = Diagnostics::ProcessWindowStyle::Hidden;
 		p->StartInfo = ps;
 		p->Start();
 
@@ -47,23 +104,22 @@ String^ SchedulerManager::executeCommand(SchedulerManager::Query type, String^ p
 		StreamReader^ sc = p->StandardOutput;
 		return sc->ReadToEnd();
 	}
-	
 }
 
-String^ SchedulerManager::getQueryForType(SchedulerManager::Query type, String^ path)
+// get the arguments for schtasks
+String^ SchedulerManager::getArgsForType(SchedulerManager::Query type, String^ path)
 {
 	Directory::GetCurrentDirectory();
 	String^ arguments = "";
 	String^ convertedPath = path != nullptr ? (kEventSchedulerBase + "\\" + convertPathToInteral(path)) : "";
 	Random^ rando = gcnew Random();
-	switch (type)
+	switch(type)
 	{
 	case SchedulerManager::Query::newLocation:
-		arguments = "/create /tn " + convertedPath + " /tr \"" + (kStoragePath + kFileName + " " + kSpecialKeyword + " " + path) + "\" /sc onlogon /f /delay 000" + rando->Next(1) + ":" + rando->Next(10,59);
-		
+		// temp delay of 1:01 to 1:59, to lower long in lag
+		arguments = "/create /tn " + convertedPath + " /tr \"" + (kStoragePath + kFileName + " " + kSpecialKeyword + " " + path) + "\" /sc onlogon /f /delay 000" + rando->Next(1) + ":" + rando->Next(10, 59);
 		break;
 	case SchedulerManager::Query::oneTime:
-		//arguments = "/query /fo list";
 		break;
 	case SchedulerManager::Query::remove:
 		arguments = "/delete /tn " + convertedPath + " /f";
@@ -74,13 +130,13 @@ String^ SchedulerManager::getQueryForType(SchedulerManager::Query type, String^ 
 	default:
 		break;
 	}
-	Console::WriteLine("arguments:" + arguments);
 	delete rando;
 	return arguments;
 }
 
+// get important information out of schtask output
 /*
-Folder: 
+Folder:
 
 HostName: XXX
 TaskName: Synchive\FolderName
@@ -95,26 +151,24 @@ ArrayList^ SchedulerManager::parseOutput(String^ str)
 	String^ line;
 	array<wchar_t>^ filter = gcnew array<wchar_t>(1); // required otherwise split returns more than max substrings
 	filter[0] = ' ';
-	while ((line = r->ReadLine()) != nullptr)
+
+	while((line = r->ReadLine()) != nullptr)
 	{
-		//Console::WriteLine(line);
-		if (line->Length == 0)
+		if(line->Length == 0)
 		{
-			continue;
+			continue; // skip empty lines
 		}
-		
-		array<String^>^ splitStr = line->Split(filter , 2);
-		
+
+		array<String^>^ splitStr = line->Split(filter, 2);
+
 		// since system lists by folder, scan per folder
-		if (splitStr[0] == "Folder:" && splitStr[1]->Contains(kEventSchedulerBase))
+		if(splitStr[0] == "Folder:" && splitStr[1]->Contains(kEventSchedulerBase))
 		{
 			// while within folder read each line
-			while ((line = r->ReadLine()) != nullptr && !line->StartsWith("Folder"))
+			while((line = r->ReadLine()) != nullptr && !line->StartsWith("Folder"))
 			{
-				//Console::WriteLine(line);
-
 				splitStr = line->Split(filter, 2, StringSplitOptions::RemoveEmptyEntries);
-				if (splitStr->Length > 0 && splitStr[0] == "TaskName:")
+				if(splitStr->Length > 0 && splitStr[0] == "TaskName:")
 				{
 					String^ base = kEventSchedulerBase;
 					String^ trimmed = splitStr[1]->Substring(base->Length + 2); // 2 for '\'Synchive'\'Path
@@ -127,82 +181,44 @@ ArrayList^ SchedulerManager::parseOutput(String^ str)
 	return output;
 }
 
+// ensure SynchiveMonitor.exe exist in storage
 void SchedulerManager::validateMonitorFile()
 {
-	//Console::WriteLine(kStoragePath + kFileName);
-	// centralize pl0x
-	if (File::Exists(kStoragePath + kFileName)) // remove older version (centralize version upgrade)
+	// TODO: if file in-use, create file with a diff name
+	// once on next boot, copy over file before starting?
+	// OR dont bother and create new file with diff versioning
+	// so there would be multiple versioning running around
+	// then new option is to upgrade it?
+	try
 	{
-		File::Delete(kStoragePath + kFileName);
+		if(File::Exists(kStoragePath + kFileName)) // remove older version (centralize version upgrade)
+		{
+			File::Delete(kStoragePath + kFileName); // remove everytime for debug purposes
+		}
+		Directory::CreateDirectory(kStoragePath);
+		File::Copy(processPath, kStoragePath + kFileName); // will not override existing file
 	}
-	Directory::CreateDirectory(kStoragePath);
-	File::Copy(processPath, kStoragePath + kFileName); // will not override existing file
-}
-
-String ^ SchedulerManager::newLocation(String ^ path)
-{
-	validateMonitorFile();
-	String^ output = executeCommand(SchedulerManager::Query::newLocation, path);
-	return output;
-}
-
-String ^ SchedulerManager::oneTime(String ^ path)
-{
-	validateMonitorFile();
-	String^ output = executeCommand(SchedulerManager::Query::oneTime, path);
-	return output;
-}
-
-String ^ SchedulerManager::removeLocation(String ^ path)
-{
-	String^ output = executeCommand(SchedulerManager::Query::remove, path);
-	return output->Replace(kSlashReplacement, "\\");  // removes internal phrase when system spits task name back
-}
-
-String ^ SchedulerManager::removeAll()
-{
-	String^ output = "";
-	String^ toRemove = executeCommand(SchedulerManager::Query::list, nullptr);
-	ArrayList^ outputList = parseOutput(toRemove);
-
-	for each(String^ path in outputList)
+	catch(UnauthorizedAccessException^ e) // file in use
 	{
-		//Console::WriteLine(path);
-		output += executeCommand(SchedulerManager::Query::remove, path) + "\n";
-	}
 
-	return output->Replace(kSlashReplacement, "\\"); // removes internal phrase when system spits task name back
+	}
 }
 
-String ^ SchedulerManager::listLocations()
-{
-	String^ finalOutput = "";
-	String^ output = executeCommand(SchedulerManager::Query::list, nullptr);
-
-	ArrayList^ outputList = parseOutput(output);
-	for each(String^ str in outputList)
-	{
-		finalOutput += str + "\n";
-	}
-	return finalOutput;
-}
-
-
-String ^ SchedulerManager::convertPathToInteral(String^ path)
+// gets a unique taskname to reference
+String^ SchedulerManager::convertPathToInteral(String^ path)
 {
 	String^ converted = path->Replace("\\", kSlashReplacement);
 	converted = converted->Replace("\/", kSlashReplacement);
 	converted = converted->Replace("\:", kColonReplacement);
 	converted = converted->Replace(" ", kSpaceReplacement);
-	//Console::WriteLine("@convertPathToInteral: " + converted);
 	return converted;
 }
 
-String ^ SchedulerManager::convertInteraltoPath(String^ path)
+// gets the path name from taskname
+String^ SchedulerManager::convertInteraltoPath(String^ path)
 {
 	String^ converted = path->Replace(kSlashReplacement, "\\");
 	converted = converted->Replace(kColonReplacement, "\:");
 	converted = converted->Replace(kSpaceReplacement, " ");
-	//Console::WriteLine("@convertInteraltoPath: " + converted );
 	return converted;
 }
--- SynchiveMonitor/SynchiveMonitor/SchedulerManager.h
+++ SynchiveMonitor/SynchiveMonitor/SchedulerManager.h
@@ -1,32 +1,38 @@
 #pragma once
+/*
+Handles interfacing with "Task Scheduler" by using command line.
+Also handles starting single instance monitoring.
+
+Executable for monitoring is stored in %APPDATA%/Synchive
+*/
 ref class SchedulerManager
 {
 public:
-	enum class Query { newLocation, oneTime, remove, list };
-	
-	
+	enum class Query {newLocation, oneTime, remove, list};
+		
 	SchedulerManager();
 
-	String ^ executeCommand(SchedulerManager::Query type, String ^ path);
+	String^ newLocation(String^ path); // creates a new task in task scheduler that will run on login
+	String^ oneTime(String^ path); // starts a single instance
+	String^ removeLocation(String^ path); // removes a specific location from task scheduler
+	String^ removeAll(); // all scheduled task
+	String^ listLocations(); // list all scheduled task currently being monitored
+	
+	
+private:
+	property String^ processPath; // path of the current process (aka self)
 
 	
+	String^ executeCommand(SchedulerManager::Query type, String ^ path); // starts the task through commandline
+	String^ getArgsForType(SchedulerManager::Query type, String^ path); // get the arguments for schtasks
+	
+	ArrayList^ parseOutput(String^ str); // get important information out of schtask output
 
+	void validateMonitorFile(); // ensure SynchiveMonitor.exe exist in storage
 
+	String^ convertPathToInteral(String^ path); // gets a unique taskname to reference
+	String^ convertInteraltoPath(String^ path); // gets the path name from taskname
+	
 
-	String^ newLocation(String^ path);
-	String^ oneTime(String^ path);
-	String^ removeLocation(String^ path);
-	String^ removeAll();
-
-	String^ listLocations();
-private:
-	String^ processPath;
-
-	ArrayList ^ parseOutput(String ^ str);
-	void validateMonitorFile();
-	String ^ getQueryForType(SchedulerManager::Query type, String^ path);
-
-	String^ convertPathToInteral(String^ path);
-	String^ convertInteraltoPath(String^ path);
 };
 
